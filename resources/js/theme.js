76a740e9c4602033b6e61c71f1a0eec9b9597cf4
Gregorio Hernández Caso <gregoriohc@gmail.com>
2018-09-17 10:21:23 +0200

    Some fixes and comments

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,30 +1,43 @@
 function load() {
+  // Set viewport
   var viewport = document.querySelector("meta[name=viewport]");
-  viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+  viewport.setAttribute('content', 'width=device-width, initial-scale=1, shrink-to-fit=no');
 
-  var contentHeader = document.querySelector('.content .h-header');
-  var hamburger = document.createElement("span");
+  // Add hidden class to sidebar
   var sidebar = document.querySelector('.w-sidebar');
   sidebar.classList.add("sidebar-hidden");
-  hamburger.html = '';
+
+  // Add hamburger menu to header
+  var hamburger = document.createElement("span");
   hamburger.className = 'hamburger-menu';
+  var contentHeader = document.querySelector('.content .h-header');
   contentHeader.prepend(hamburger);
 
-  hamburger.addEventListener("click", function () {
+  // Hamburger click event
+  hamburger.addEventListener("click", function (e) {
+    e.stopPropagation();
     var sidebar = document.querySelector('.w-sidebar');
     sidebar.classList.toggle("sidebar-hidden");
   }, true);
 
+  // Sidebar links click event
   var sidebarLinks = document.querySelectorAll('.w-sidebar a, .w-sidebar .cursor-pointer');
   sidebarLinks.forEach(function(sidebarLink) {
     sidebarLink.addEventListener("click", function() {
       var sidebar = document.querySelector('.w-sidebar');
-      var h = document.querySelector('.hamburger-menu');
-      var hStyles = getComputedStyle(h);
-      if (hStyles.getPropertyValue('display') === "block") {
+      sidebar.classList.add("sidebar-hidden");
+    }, false);
+  });
+
+  // Hide sidebar when clicking outside
+  var rootElements = document.querySelectorAll('body,html');
+  rootElements.forEach(function(rootElement) {
+    rootElement.addEventListener("click", function(e) {
+      var sidebar = document.querySelector('.w-sidebar');
+      if (e.target !== sidebar && !sidebar.contains(e.target)) {
         sidebar.classList.add("sidebar-hidden");
       }
-    }, false);
+    });
   });
 }
 
